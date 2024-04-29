5286ab49216a4b0e87242908b776ec2287e37d59
Yue Gao <hologerry@gmail.com>
2024-04-30 06:06:46 +0800

    update

---
+++ zero123/train_aml.sh
@@ -0,0 +1,9 @@
+python main.py \
+    -t \
+    --base configs/sd-scalar-flow-finetune-c_concat-256_aml.yaml \
+    --gpus 0,1,2,3 \
+    --scale_lr False \
+    --num_nodes 1 \
+    --seed 42 \
+    --check_val_every_n_epoch 10 \
+    --finetune_from ../../zero123-weights/zero123-xl.ckpt \
